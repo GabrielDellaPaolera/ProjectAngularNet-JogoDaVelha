72c9f77c2033a28da97a306aebe513cd761ce7f9
Gabriel Perussolo Della Paolera <gabriel.paolera@gsoftware.com.br>
2025-07-09 14:06:15 -0300

    feature/readme-git

--- backend/Models/Resultado.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Models
-{
-    public class Resultado
-    {
-        public int Id { get; set; }
-        public string Vencedor { get; set; } = string.Empty; // "X", "O" ou "E"
-        public DateTime DataHora { get; set; }
-    }
-}
